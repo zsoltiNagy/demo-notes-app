f5131751a3711a04ee1276f9bffc59a2f2999e92
zsoltiNagy <zsoltiusz.nagy@gmail.com>
2023-06-23 11:00:32 +0200

    Adding atimeout

--- packages/functions/src/get.js
+++ packages/functions/src/get.js
@@ -2,9 +2,11 @@ export const main = handler(async (event) => {
   const params = {
     TableName: Table.Notes.tableName,
     // 'Key' defines the partition key and sort key of the item to be retrieved
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
     Key: {
       userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-      // noteId: event.pathParameters.id, // The id of the note from the path
+      noteId: event.pathParameters.id,
     },
   };
 
@@ -12,6 +14,9 @@ export const main = handler(async (event) => {
   if (!result.Item) {
     throw new Error("Item not found.");
   }
+
+  // Set a timeout
+  await new Promise((resolve) => setTimeout(resolve, 10000));
 
   // Return the retrieved item
   return result.Item;
